92a26c3a83c43e9140f960e501fda6a351aaa8dd
mjhornbuckle@gmail.com <magpie@warbler.(none)>
2009-08-22 14:20:31 -0600

    Error w/ friendship controller fix

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,11 +1,10 @@
 class FriendshipsController < ApplicationController
 
   def create
-    session[:user_id] = User.find(:first).user_id
     @current_user = User.find(session[:user_id])
 
     friendship = @current_user.friendships.build(:friend_id => params[:friend_id])  
-    if @friendship.save  
+    if friendship.save  
       flash[:notice] = "Added friend."  
     else  
       flash[:notice] = "Unable to add friend."  
