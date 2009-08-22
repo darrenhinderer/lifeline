22531bc30772375d2d07471856e619d447527469
TerryHowe <terrylhowe@gmail.com>
2009-08-21 21:46:03 -0600

    capify

---
+++ config/deploy.rb
@@ -0,0 +1,48 @@
+default_run_options[:pty] = true
+set :application, "rr09-team-199"
+set :repository, "git@github.com:railsrumble/rr09-team-199.git"
+
+role :app, "magpie.r09.railsrumble.com"
+role :web, "magpie.r09.railsrumble.com"
+role :db, "magpie.r09.railsrumble.com", :primary => true
+
+set :deploy_to, "/home/magpie/#{application}"
+set :scm, :git
+set :branch, "master"
+set :user, "magpie"
+
+after "deploy:finalize_update", "db:symlink"
+
+namespace :deploy do
+   desc "restart passenger"
+   task :restart, :roles => :app, :except => {:no_release => true} do
+      run "touch #{current_path}/tmp/restart.txt"
+   end
+
+   [:start, :stop].each do |t|
+      desc "#{t} task is a no-op with passenger"
+      task t, :roles => :app do ; end
+   end
+end 
+
+namespace :db do
+  task :setup, :except => {:no_release => true} do 
+    default_template = <<-EOF
+      production:
+        adapter: mysql
+        encoding: utf8
+        database: magpie
+        pool: 5
+        username: magpie
+        password: magpie
+        socket: /var/run/mysqld/mysqld.sock
+    EOF
+
+    config = ERB.new(default_template)
+    put config.result(binding), "#{shared_path}/database.yml"
+  end
+
+  task :symlink, :except => {:no_release => true} do
+    run "ln -nfs #{shared_path}/database.yml #{release_path}/config/database.yml"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
+  map.root :controller => :events
   map.resources :events
 
   # The priority is based upon order of creation: first created -> highest priority.
