b81762094d79ddc02c676c72588d81ac72e039fa
TerryHowe <terrylhowe@gmail.com>
2009-08-21 23:30:40 -0600

    Added fields to event class

--- db/migrate/001_create_events.rb
+++ db/migrate/001_create_events.rb
@@ -2,7 +2,11 @@ class CreateEvents < ActiveRecord::Migration
   def self.up
     create_table :events do |t|
       t.integer :user_id
-      t.timestamps
+      t.string :title
+      t.string :content, 	:limit => 500
+      t.datetime :start_date
+      t.datetime :end_date
+      t.boolean :private,	:default => 0
     end
   end
 
