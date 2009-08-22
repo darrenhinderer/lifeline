f7bbe3ab8fcd91367eb9cb778064ccf3440d1642
Greg <greg@dodo.(none)>
2009-08-21 22:46:55 -0600

    Greg: initial simile (remote)

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,28 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+var tl;
+function onLoad() {
+  var bandInfos = [
+    Timeline.createBandInfo({
+        width:          "70%", 
+        intervalUnit:   Timeline.DateTime.MONTH, 
+        intervalPixels: 100
+    }),
+    Timeline.createBandInfo({
+        width:          "30%", 
+        intervalUnit:   Timeline.DateTime.YEAR, 
+        intervalPixels: 200
+    })
+  ];
+  bandInfos[1].syncWith = 0;
+  bandInfos[1].highlight = true;
+  tl = Timeline.create(document.getElementById("lifeline"), bandInfos);
+}
+
+var resizeTimerID = null;
+function onResize() {
+    if (resizeTimerID == null) {
+        resizeTimerID = window.setTimeout(function() {
+            resizeTimerID = null;
+            tl.layout();
+        }, 500);
+    }
+}
